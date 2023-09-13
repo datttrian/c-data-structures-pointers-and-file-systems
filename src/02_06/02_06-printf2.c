d73a3c67e3527fc68f7f57764be2b44d5c4f6b36
datttrian <datttrian@gmail.com>
2023-09-13 20:29:59 +0000

    Sending string output

---
+++ src/02_06/02_06-printf2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, Earth!\n");
+	printf("%s", "Hello, Moon!\n");
+
+	return (0);
+}
---
+++ src/02_06/02_06-puts1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	char string[] = "Yet another string literal";
+
+	puts("A string literal");
+	puts(string);
+
+	return (0);
+}
