744254396d03c91e6fd1769fda606591ba306b84
datttrian <datttrian@gmail.com>
2023-09-13 18:10:47 +0000

    Passing an array to a function

--- src/01_04/01_04-returnarray1.c
+++ src/01_04/01_04-returnarray1.c
@@ -2,15 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-int makearray[](void)
+// int makearray[](void)
+// {
+// 	int static array[5];
+// 	int x;
+
+// 	for (x = 0; x < 5; x++)
+// 		array[x] = rand() % 10 + 1;
+
+// 	return (array);
+// }
+
+void makearray(int *array, int size)
 {
-	int static array[5];
-	int x;
-
-	for (x = 0; x < 5; x++)
+	for (int x = 0; x < size; x++)
 		array[x] = rand() % 10 + 1;
-
-	return (array);
 }
 
 int main()
@@ -19,7 +25,9 @@ int main()
 
 	srand((unsigned)time(NULL));
 	puts("Here are your 5 random numbers:");
-	r = makearray();
+	// r = makearray();
+	makearray(&r[0], 5); // Pass the address of the first element of the array
+
 	for (x = 0; x < 5; x++)
 		printf("%d\n", r[x]);
 
