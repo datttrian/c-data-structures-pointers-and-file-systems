cc5bde5b2f314ddef14b19a6f64490ca7bfb43f9
datttrian <datttrian@gmail.com>
2023-09-13 18:42:16 +0000

    Using single character I/O

---
+++ src/02_01/02_01-putchar2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	putc('H', stdout);
+	putc('e', stdout);
+	putc('l', stdout);
+	putc('l', stdout);
+	putc('o', stdout);
+	putc('\n', stdout);
+
+	return (0);
+}
