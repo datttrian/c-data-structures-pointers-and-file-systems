45671ff3f865422a9e418d959f5e7ddc39fe34bf
datttrian <datttrian@gmail.com>
2023-09-14 21:49:21 +0000

    Format files

--- src/04_04/04_04-readrand1.c
+++ src/04_04/04_04-readrand1.c
@@ -3,6 +3,7 @@
 int main()
 {
 	const char filename[] = "presidents.dat";
+
 	struct person
 	{
 		char name[32];
@@ -12,7 +13,7 @@ int main()
 	int x;
 	FILE *fp;
 
-	/* open the file */
+	// Open the file
 	fp = fopen(filename, "r");
 	if (fp == NULL)
 	{
@@ -20,28 +21,26 @@ int main()
 		return (1);
 	}
 
-	/* prompt to read a specific record */
+	// Prompt to read a specific record
 	printf("Enter record to read (1-10): ");
 	scanf("%d", &x);
-	/* validate the input */
+
+	// Validate the input
 	if (x < 1 || x > 10)
 	{
 		puts("Invalid input");
 		return (1);
 	}
-	/* decrement x to reference the proper offset */
+
+	// Decrement x to reference the proper offset
 	x--;
 
-	/* Set the file position indicator */
+	// Set the file position indicator
 	fseek(fp, sizeof(struct person) * x, SEEK_SET);
-	/* fetch the record */
+
+	// Fetch the record
 	fread(&president, sizeof(struct person), 1, fp);
 
-	/* printthe result */
-	printf("President %s was %d years old when inaugurated in %d\n",
-		   president.name,
-		   president.age,
-		   president.inauguration);
-
-	return (0);
+	// Print the result
+	printf("President %s was %d years old when inaugurated in %d\n", president.name, president.age, president.inauguration);
 }
